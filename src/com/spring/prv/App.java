16799e26c77359c4ce4eaf1cee9c559865f007da
Chandrasekar <chandrasekar492@gmail.com>
2017-05-19 05:03:01 +0530

    updated to spring project

--- src/com/spring/prv/App.java
+++ src/com/spring/prv/App.java
@@ -1,10 +1,15 @@
 package com.spring.prv;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@RequestMapping("/")
+	public String getHome(){
+		
+		return "home";
 	}
-
+	
 }
