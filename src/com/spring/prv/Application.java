32de1e7b01c9bbf36f24885155783317a056469b
475139 <475139@PC362612.cts.com>
2018-01-22 17:43:11 +0530

    change to new model

--- src/com/spring/prv/Application.java
+++ src/com/spring/prv/Application.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class App {
+public class Application {
 	
 	//private HomeSession sess;
 
